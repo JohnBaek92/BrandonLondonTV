972872d739143a50063f5542247f0b53881c5904
John Baek <johnbaek92@gmail.com>
2018-01-15 17:56:51 -0500

    remove height from header

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -32754,7 +32754,7 @@ var About = function About() {
 							"article",
 							{ className: "tile is-child" },
 							_react2.default.createElement(
-								"p",
+								"div",
 								{ className: "subtitle" },
 								"As an athlete, actor, and broadcast personality, Brandon London is well on his way to becoming the next triple threat in the industry. Known as the \u201CCultured Athlete\u201D, London combines his talent and charisma on and off the football field. After being signed to the NY Giants in 2006 as a free agent, London acquired his first SuperBowl ring."
 							)
@@ -32762,9 +32762,9 @@ var About = function About() {
 						_react2.default.createElement(
 							"article",
 							{ className: "tile is-child notification is-warning" },
-							_react2.default.createElement("p", { className: "title" }),
+							_react2.default.createElement("div", { className: "title" }),
 							_react2.default.createElement(
-								"p",
+								"div",
 								{ className: "title" },
 								"\u201CBrandon London is a tall drink of personality. He is a generous host with a genius brand\u201D -Marki Costello"
 							)
@@ -32787,7 +32787,7 @@ var About = function About() {
 						"article",
 						{ className: "tile is-child" },
 						_react2.default.createElement(
-							"p",
+							"div",
 							{ className: "subtitle" },
 							"From NY, he then headed to the Miami Dolphins as a reserve receiver. Mainly used as Special Teams contributor, he also made his mark at Wide Receiver posting a team high of 4 catches in the first round playoffs vs. the Baltimore Ravens. In 2010, London signed with the Canadian Football League\u2019s Montreal Alouettes and won a Grey Cup Championship (equivalent to the NFL\u2019s SuperBowl). He is one of 6 players to win both a Super Bowl and Grey Cup championship."
 						),
@@ -32804,40 +32804,40 @@ var About = function About() {
 					),
 					_react2.default.createElement(
 						"div",
-						{ className: "tile is_child is-7 level-right", style: { padding: "0 0 0 0.8em;" } },
+						{ className: "tile is_child is-7 level-right", style: { padding: "0 0 0 0.8em" } },
 						_react2.default.createElement(
-							"p",
+							"div",
 							{ className: "subtitle" },
 							_react2.default.createElement(
 								"div",
 								null,
 								_react2.default.createElement(
-									"p",
+									"div",
 									{ className: "subtitle" },
 									"Originally from Richmond, Virginia, Brandon grew up learning about football firsthand from his father, who is a respected college football coach at the University of Virginia. Upon high school graduation, Brandon played football and studied sociology at the University of Massachusetts. While at UMass, he broke records in career catches and touchdowns. In 2008, Brandon appeared on BET\u2019s \u201CRip the Runway\u201D hosted by Sean \u201CDiddy\u201D Combs."
 								),
 								_react2.default.createElement(
-									"p",
+									"div",
 									{ className: "subtitle" },
 									"It was there that Brandon discovered his passion to act and host, aside from his natural athletic talent. With an irresistible charm, unforgettable personality and business savvy that rivals entertainment industry veterans; Brandon London is becoming one of the most sought after personalities on the red carpet."
 								),
 								_react2.default.createElement(
-									"p",
+									"div",
 									{ className: "subtitle" },
 									"Upon arriving in Los Angeles in 2013 during his off season, Brandon joined entertainment series, AfterBuzzTV created by Maria Menounos (Extra TV) as a weekly co-host summarizing events that occur on NBC\u2019s hit series, Revolution and BET's 'The Game.'"
 								),
 								_react2.default.createElement(
-									"p",
+									"div",
 									{ className: "subtitle" },
 									"Launching his own series \u201CJersey Off, Suit On\u201D exemplifies his persona as a football star and entertainer. This series allows the viewer to learn about fashion, music, and football from \u201CThe Cultured Athlete\u201D himself."
 								),
 								_react2.default.createElement(
-									"p",
+									"div",
 									{ className: "subtitle" },
 									"For the past 5 years, London has taken his on field knowledge to train and help mentor many of the top high school football players along the east and west coast, Brandon is committed to using his celebrity status for more than red carpets and exclusive events. He is committed to empowering the youth and encourage them to follow their dreams as a positive role model."
 								),
 								_react2.default.createElement(
-									"p",
+									"div",
 									{ className: "subtitle" },
 									"Brandon is well on his way to becoming the next leading multimedia brand and he is very clear that his passion isn\u2019t solely about making money it\u2019s about making history!"
 								)
--- frontend/components/about.jsx
+++ frontend/components/about.jsx
@@ -9,21 +9,21 @@ const About = () => {
 					<div className="tile">
 						<div className="tile is-parent is-vertical">
 							<article className="tile is-child">
-								<p className="subtitle">
+								<div className="subtitle">
 									As an athlete, actor, and broadcast personality, Brandon
 									London is well on his way to becoming the next triple threat
 									in the industry. Known as the “Cultured Athlete”, London
 									combines his talent and charisma on and off the football
 									field. After being signed to the NY Giants in 2006 as a free
 									agent, London acquired his first SuperBowl ring.
-								</p>
+								</div>
 							</article>
 							<article className="tile is-child notification is-warning">
-								<p className="title" />
-								<p className="title">
+								<div className="title" />
+								<div className="title">
 									“Brandon London is a tall drink of personality. He is a
 									generous host with a genius brand” -Marki Costello
-								</p>
+								</div>
 							</article>
 						</div>
 						<div className="tile is-parent">
@@ -34,7 +34,7 @@ const About = () => {
 					</div>
 					<div className="tile is-parent">
 						<article className="tile is-child">
-							<p className="subtitle">
+							<div className="subtitle">
 								From NY, he then headed to the Miami Dolphins as a reserve
 								receiver. Mainly used as Special Teams contributor, he also made
 								his mark at Wide Receiver posting a team high of 4 catches in
@@ -43,7 +43,7 @@ const About = () => {
 								Alouettes and won a Grey Cup Championship (equivalent to the
 								NFL’s SuperBowl). He is one of 6 players to win both a Super
 								Bowl and Grey Cup championship.
-							</p>
+							</div>
 							<div className="content" />
 						</article>
 					</div>
@@ -51,10 +51,10 @@ const About = () => {
 						<div className="tile is_child is-5 level-left">
 							<img src={window.images.smiling} />
 						</div>
-						<div className="tile is_child is-7 level-right" style={{padding: "0 0 0 0.8em;"}}>
-							<p className="subtitle">
+						<div className="tile is_child is-7 level-right" style={{padding: "0 0 0 0.8em"}}>
+							<div className="subtitle">
 								<div>
-									<p className="subtitle">
+									<div className="subtitle">
 										Originally from Richmond, Virginia, Brandon grew up learning
 										about football firsthand from his father, who is a respected
 										college football coach at the University of Virginia. Upon
@@ -63,30 +63,30 @@ const About = () => {
 										UMass, he broke records in career catches and touchdowns. In
 										2008, Brandon appeared on BET’s “Rip the Runway” hosted by
 										Sean “Diddy” Combs.
-									</p>
+									</div>
 
-									<p className="subtitle">
+									<div className="subtitle">
 										It was there that Brandon discovered his passion to act and
 										host, aside from his natural athletic talent. With an
 										irresistible charm, unforgettable personality and business
 										savvy that rivals entertainment industry veterans; Brandon
 										London is becoming one of the most sought after
 										personalities on the red carpet.
-									</p>
-									<p className="subtitle">
+									</div>
+									<div className="subtitle">
 										Upon arriving in Los Angeles in 2013 during his off season,
 										Brandon joined entertainment series, AfterBuzzTV created by
 										Maria Menounos (Extra TV) as a weekly co-host summarizing
 										events that occur on NBC’s hit series, Revolution and BET's
 										'The Game.'
-									</p>
-									<p className="subtitle">
+									</div>
+									<div className="subtitle">
 										Launching his own series “Jersey Off, Suit On” exemplifies his
 										persona as a football star and entertainer. This series allows
 										the viewer to learn about fashion, music, and football from
 										“The Cultured Athlete” himself.
-									</p>
-									<p className="subtitle">
+									</div>
+									<div className="subtitle">
 										For the past 5 years, London has taken his on field knowledge
 										to train and help mentor many of the top high school football
 										players along the east and west coast, Brandon is committed to
@@ -94,14 +94,14 @@ const About = () => {
 										exclusive events. He is committed to empowering the youth and
 										encourage them to follow their dreams as a positive role
 										model.
-									</p>
-									<p className="subtitle">
+									</div>
+									<div className="subtitle">
 										Brandon is well on his way to becoming the next leading
 										multimedia brand and he is very clear that his passion isn’t
 										solely about making money it’s about making history!
-									</p>
+									</div>
 								</div>
-							</p>
+							</div>
 						</div>
 					</div>
 				</div>
