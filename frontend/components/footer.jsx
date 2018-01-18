127e498088e869feca13a8cd7eb62d72920dfde7
Wadah Adlan <wadahadlan@gmail.com>
2018-01-18 00:41:47 -0500

    Added Icons to Footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -2,25 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-	return (
-		<footer className="footer">
-			<div className="container">
-				<nav>
-					<div className="nav-left navbar-item" />
-					<div className="nav-center">
-						<Link to="/">
-							<img
-								className="footer-logo"
-								style={{ height: "13em", width: "13em" }}
-								src={window.images.circle_logo}
-							/>
-						</Link>
-					</div>
-					<div className="nav-right navbar-item" />
-				</nav>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="footer">
+      <div className="container">
+        <nav>
+          <div className="nav-left navbar-item" />
+          <div className="nav-center middle-centered">
+            <div className="social-tag">
+              <i className="fa fa-3x fa-facebook" />
+            </div>
+            <div className="social-tag">
+              <i className="fa fa-3x fa-twitter" />
+            </div>
+            <Link to="/">
+              <img
+                className="footer-logo"
+                style={{ height: "13em", width: "13em" }}
+                src={window.images.circle_logo}
+              />
+            </Link>
+            <div className="social-tag">
+              <i className="fa fa-3x fa-youtube" />
+            </div>
+            <div className="social-tag">
+              <i className="fa fa-3x fa-instagram" />
+            </div>
+          </div>
+          <div className="nav-right navbar-item" />
+        </nav>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
