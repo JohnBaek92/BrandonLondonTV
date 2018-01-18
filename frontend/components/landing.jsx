fb50bbddb334549f6ba0a7dca04148ff3c59aba8
John Baek <johnbaek92@gmail.com>
2018-01-18 15:05:04 -0500

    add JOSO logo to landing and increase font-size of top header

--- frontend/components/landing.jsx
+++ frontend/components/landing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Timeline } from "react-twitter-widgets";
 import Instagram from "./instagram_feed";
 import Hero from "./hero";
+import { Link } from "react-router-dom"
 
 class Landing extends React.Component {
 	constructor(props) {
@@ -9,30 +10,20 @@ class Landing extends React.Component {
   }
 
 	render() {
-		return (
-			<div className="container">
-				<Hero />
-				<div className="block social-embed columns">
-					<div className="column">
-						<Timeline
-							dataSource={{
-								sourceType: "profile",
-								screenName: "BrandonLondonTV"
-							}}
-							options={{
-								username: "BrandonLondonTV",
-								height: "70vh",
-								theme: "dark",
-                chrome: "noscrollbar"
-							}}
-						/>
-					</div>
-					<div className="column">
-						<Instagram />
-					</div>
-				</div>
-			</div>
-		);
+		return <div className="container">
+        <Hero />
+        <div className="block social-embed columns">
+          <div className="column">
+            <Timeline dataSource={{ sourceType: "profile", screenName: "BrandonLondonTV" }} options={{ username: "BrandonLondonTV", height: "70vh", theme: "dark", chrome: "noscrollbar" }} />
+          </div>
+          <div className="column">
+            <Instagram />
+          </div>
+        </div>
+        <div style={{ background: "#030331" }}>
+          <Link to="/joso"><img src={window.images.joso_yellow} style={{ width: "25%", display: "block", margin: "0 auto" }} /></Link>
+        </div>
+      </div>;
 	}
 }
 
