cc74d021710087ba0181d5cf43a276ac1ffc404d
Wadah Adlan <wadahadlan@gmail.com>
2018-01-25 19:14:54 -0500

    New Logo on Footer, Dynamic Videos v.5

--- frontend/components/entertainment.jsx
+++ frontend/components/entertainment.jsx
@@ -1,42 +1,90 @@
-import React from 'react';
+import React from "react";
+import Youtube from "react-youtube";
 
-const photoStyle = {width: "15%", height: "15%"};
+const photoStyle = { width: "15%", height: "15%" };
 
-const Entertainment = () => {
-  return <section className="entertainment-wrapper">
-      <div className="title has-text-centered">
-        Photos
+class Entertainment extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      top: "35vh",
+      middle: "25vh",
+      bottom: "15vh"
+    };
+
+    this.videos = [
+      "E8JlWMXJgqA",
+      "BKXLB4-9sfg",
+      "KLmwpPbTsN0",
+      "rJ_qxnuKADY",
+      "YbXrdVjzkC4",
+      "AOmHwHWpdKc",
+      "erRJtA8bkVc",
+      "WSmNvV-8uwU"
+    ];
+    this.changeHeight = this.changeHeight.bind(this);
+    this.makeVideo = this.makeVideo.bind(this);
+  }
+
+  changeHeight(row) {
+    const newState = Object.assign({}, this.state);
+    newState.top = "15vh";
+    newState.middle = "15vh";
+    newState.bottom = "15vh";
+    newState[row] = "50vh";
+    debugger
+    // this.setState(newState);
+    this.setState(newState);
+  }
+
+  makeVideo(link, row) {
+    return (
+      <div className="column" key={link}>
+        <Youtube
+          videoId={link}
+          opts={{
+            height: "100%",
+            width: "100%",
+            playerVars: {
+              // https://developers.google.com/youtube/player_parameters
+              autoplay: 0
+            }
+          }}
+          onPlay={() => this.changeHeight(row)}
+        />
       </div>
-      <div className="columns">
-        <img className="column" style={photoStyle} src={window.images.e1} />
-        <img className="column" style={photoStyle} src={window.images.e5} />
-        <img className="column" style={photoStyle} src={window.images.e3} />
-        <img className="column" style={photoStyle} src={window.images.e4} />
-      </div>
-      <div className="columns">
-        <img className="column" style={photoStyle} src={window.images.e2} />
-        <img className="column" style={photoStyle} src={window.images.e6} />
-        <img className="column" style={photoStyle} src={window.images.e7} />
-        <img className="column" style={photoStyle} src={window.images.e8} />
-      </div>
-      <div className="title has-text-centered">
-        Videos
-      </div>
-      <div className="columns" style={{height: "35vh"}}>
-        <iframe className="column" allowFullScreen="true" width="40%" height="auto" frameBorder="0" src="https://www.youtube.com/embed/E8JlWMXJgqA" />
-        <iframe className="column" allowFullScreen="true" width="40%" height="auto" frameBorder="0" src="https://www.youtube.com/embed/BKXLB4-9sfg" />
-      </div>
-      <div className="columns">
-        <iframe className="column" allowFullScreen="true" width="40%" height="60%" frameBorder="0" src="https://www.youtube.com/embed/KLmwpPbTsN0" />
-        <iframe className="column" allowFullScreen="true" width="40%" height="60%" frameBorder="0" src="https://www.youtube.com/embed/rJ_qxnuKADY" />
-      </div>
-      <div className="columns">
-        <iframe className="column" allowFullScreen="true" width="20%" height="20%" frameBorder="0" src="https://www.youtube.com/embed/YbXrdVjzkC4" />
-        <iframe className="column" allowFullScreen="true" width="20%" height="20%" frameBorder="0" src="https://www.youtube.com/embed/AOmHwHWpdKc" />
-        <iframe className="column" allowFullScreen="true" width="20%" height="20%" frameBorder="0" src="https://www.youtube.com/embed/erRJtA8bkVc" />
-        <iframe className="column" allowFullScreen="true" width="20%" height="20%" frameBorder="0" src="https://www.youtube.com/embed/WSmNvV-8uwU" />
-      </div>
-    </section>;
-};
+    );
+  }
+
+  render() {
+    return (
+      <section className="entertainment-wrapper">
+        <div className="title has-text-centered">Photos</div>
+        <div className="columns">
+          <img className="column" style={photoStyle} src={window.images.e1} />
+          <img className="column" style={photoStyle} src={window.images.e5} />
+          <img className="column" style={photoStyle} src={window.images.e3} />
+          <img className="column" style={photoStyle} src={window.images.e4} />
+        </div>
+        <div className="columns">
+          <img className="column" style={photoStyle} src={window.images.e2} />
+          <img className="column" style={photoStyle} src={window.images.e6} />
+          <img className="column" style={photoStyle} src={window.images.e7} />
+          <img className="column" style={photoStyle} src={window.images.e8} />
+        </div>
+        <div className="title has-text-centered">Videos</div>
+        <div className="columns" style={{ height: this.state.top }}>
+          {this.videos.slice(0, 2).map(link => this.makeVideo(link, "top"))}
+        </div>
+        <div className="columns" style={{ height: this.state.middle }}>
+          {this.videos.slice(2, 4).map(link => this.makeVideo(link, "middle"))}
+        </div>
+        <div className="columns" style={{ height: this.state.bottom }}>
+          {this.videos.slice(4, 8).map(link => this.makeVideo(link, "bottom"))}
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Entertainment;
--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -2,31 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <footer className="footer">
+  return (
+    <footer className="footer">
       <div className="container">
         <nav>
           <div className="nav-left navbar-item" />
           <div className="nav-center middle-centered">
             <div className="social-tag">
-              <a className="social-tag" href="https://www.facebook.com/BrandonLondonTv/" target="_blank">
+              <a
+                className="social-tag"
+                href="https://www.facebook.com/BrandonLondonTv/"
+                target="_blank"
+              >
                 <i className="fa fa-3x fa-facebook" />
               </a>
             </div>
             <div className="social-tag">
-              <a className="social-tag" href="https://twitter.com/brandonlondontv" target="_blank">
+              <a
+                className="social-tag"
+                href="https://twitter.com/brandonlondontv"
+                target="_blank"
+              >
                 <i className="fa fa-3x fa-twitter" />
               </a>
             </div>
-            <Link to="/">
-              <img className="footer-logo" src={window.images.circle_logo} />
-            </Link>
+            <div className="social-tag med">
+              <Link to="/">
+                <img className="footer-logo" src={window.images.circle_logo} />
+              </Link>
+            </div>
             <div className="social-tag">
-              <a className="social-tag" href="https://www.youtube.com/user/TheCulturedAthlete" target="_blank">
+              <a
+                className="social-tag"
+                href="https://www.youtube.com/user/TheCulturedAthlete"
+                target="_blank"
+              >
                 <i className="fa fa-3x fa-youtube" />
               </a>
             </div>
             <div className="social-tag">
-              <a className="social-tag" href="https://www.instagram.com/brandonlondontv/" target="_blank">
+              <a
+                className="social-tag"
+                href="https://www.instagram.com/brandonlondontv/"
+                target="_blank"
+              >
                 <i className="fa fa-3x fa-instagram" />
               </a>
             </div>
@@ -34,7 +53,8 @@ const Footer = () => {
           <div className="nav-right navbar-item" />
         </nav>
       </div>
-    </footer>;
+    </footer>
+  );
 };
 
 export default Footer;
