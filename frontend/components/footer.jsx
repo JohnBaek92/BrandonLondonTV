0d4cfba0f53ec70cdfc55d2a3ccb2ca2154ce7de
Wadah Adlan <wadahadlan@gmail.com>
2018-01-28 23:07:39 -0500

    Footer Logo is now a css ID

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,60 +1,82 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="container">
-        <nav>
-          <div className="nav-left navbar-item" />
-          <div className="nav-center middle-centered">
-            <div className="social-tag">
-              <a
-                className="social-tag"
-                href="https://www.facebook.com/BrandonLondonTv/"
-                target="_blank"
-              >
-                <i className="fa fa-3x fa-facebook" />
-              </a>
+class Footer extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = { image: window.images.circle_logo };
+
+    this.changeImage = this.changeImage.bind(this);
+    this.changeBack = this.changeBack.bind(this);
+  }
+
+  changeImage() {
+    this.setState({ image: window.images.circle_logo2 });
+  }
+
+  changeBack() {
+    this.setState({ image: window.images.circle_logo });
+  }
+
+  render() {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <nav>
+            <div className="nav-left navbar-item" />
+            <div className="nav-center middle-centered">
+              <div className="social-tag">
+                <a
+                  className="social-tag"
+                  href="https://www.facebook.com/BrandonLondonTv/"
+                  target="_blank"
+                >
+                  <i className="fa fa-3x fa-facebook" />
+                </a>
+              </div>
+              <div className="social-tag">
+                <a
+                  className="social-tag"
+                  href="https://twitter.com/brandonlondontv"
+                  target="_blank"
+                >
+                  <i className="fa fa-3x fa-twitter" />
+                </a>
+              </div>
+              <div id="footer-logo">
+                <Link
+                  to="/"
+                  onMouseEnter={this.changeImage}
+                  onMouseLeave={this.changeBack}
+                >
+                  <img className="footer-logo" src={this.state.image} />
+                </Link>
+              </div>
+              <div className="social-tag">
+                <a
+                  className="social-tag"
+                  href="https://www.youtube.com/user/TheCulturedAthlete"
+                  target="_blank"
+                >
+                  <i className="fa fa-3x fa-youtube" />
+                </a>
+              </div>
+              <div className="social-tag">
+                <a
+                  className="social-tag"
+                  href="https://www.instagram.com/brandonlondontv/"
+                  target="_blank"
+                >
+                  <i className="fa fa-3x fa-instagram" />
+                </a>
+              </div>
             </div>
-            <div className="social-tag">
-              <a
-                className="social-tag"
-                href="https://twitter.com/brandonlondontv"
-                target="_blank"
-              >
-                <i className="fa fa-3x fa-twitter" />
-              </a>
-            </div>
-            <div className="social-tag med">
-              <Link to="/">
-                <img className="footer-logo" src={window.images.circle_logo} />
-              </Link>
-            </div>
-            <div className="social-tag">
-              <a
-                className="social-tag"
-                href="https://www.youtube.com/user/TheCulturedAthlete"
-                target="_blank"
-              >
-                <i className="fa fa-3x fa-youtube" />
-              </a>
-            </div>
-            <div className="social-tag">
-              <a
-                className="social-tag"
-                href="https://www.instagram.com/brandonlondontv/"
-                target="_blank"
-              >
-                <i className="fa fa-3x fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <div className="nav-right navbar-item" />
-        </nav>
-      </div>
-    </footer>
-  );
-};
+            <div className="nav-right navbar-item" />
+          </nav>
+        </div>
+      </footer>
+    );
+  }
+}
 
 export default Footer;
