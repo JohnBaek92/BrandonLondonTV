c1f2f88e377b3f64dcd5244c60c748637c8325f9
John Baek <johnbaek92@gmail.com>
2018-02-02 18:58:35 -0500

    change font for london athletic

--- frontend/components/london_athletic.jsx
+++ frontend/components/london_athletic.jsx
@@ -5,49 +5,42 @@ class LondonAthletic extends React.Component {
     window.scrollTo(0, 0);
   }
   render() {
-    return (
-      <div className="london-athletics-wrapper">
+    return <div className="london-athletics-wrapper">
         <div className="box">
           <div className="london-athletic-video-wrapper">
-            <iframe
-              className="london-athletic-video"
-              width="700px"
-              height="380px"
-              frameBorder="0"
-              src="https://www.youtube.com/embed/qUNDomDbYtI"
-            />
+            <iframe className="london-athletic-video" width="700px" height="380px" frameBorder="0" src="https://www.youtube.com/embed/qUNDomDbYtI" />
           </div>
           <div>
             <div className="london-athletic-text padding-bottom-london-athletic">
-              Through the experience of training for a professional football,
-              Brandon London has positioned himself as one of the top Personal
-              Trainer/Sports Trainers in the nation. With workouts ranging from
-              off-season sports specific training to fitness bootcamps, London
-              Athletic appeals to all.
+              Through the experience of training for a professional
+              football, Brandon London has positioned himself as one of the
+              top Personal Trainer/Sports Trainers in the nation. With
+              workouts ranging from off-season sports specific training to
+              fitness bootcamps, London Athletic appeals to all.
             </div>
             <div className="flex-direction-col">
-              <div className="padding-bottom-london-athletic">
+              <div className="padding-bottom-london-athletic font-10px">
                 • Strength & Conditioning for Athletes
               </div>
-              <div className="padding-bottom-london-athletic">
+              <div className="padding-bottom-london-athletic font-10px">
                 • TRX Body Sculpt Classes
               </div>
-              <div className="padding-bottom-london-athletic">
+              <div className="padding-bottom-london-athletic font-10px">
                 • Fitness Bootcamps
               </div>
-              <div className="padding-bottom-london-athletic">
+              <div className="padding-bottom-london-athletic font-10px">
                 • Celebrity Training
               </div>
-              <div className="padding-bottom-london-athletic">
+              <div className="padding-bottom-london-athletic font-10px">
                 • NFL Combine Training
               </div>
-              <div className="padding-bottom-london-athletic">
+              <div className="padding-bottom-london-athletic font-10px">
                 • Offensive Pass Game Training
               </div>
-              <div className="padding-bottom-london-athletic">
+              <div className="padding-bottom-london-athletic font-10px">
                 • Power Plate Training
               </div>
-              <div className="padding-bottom-london-athletic">
+              <div className="padding-bottom-london-athletic font-10px">
                 • Train Like An Athlete Fitness Workout
               </div>
             </div>
@@ -58,34 +51,12 @@ class LondonAthletic extends React.Component {
             Catches Clinics - Pass Game Development - Lineman Work
           </div>
           <div className="block flex-row-center">
-            <iframe
-              width="500px"
-              height="400px"
-              frameBorder="0"
-              src="https://www.youtube.com/embed/wk5GSPw_zX8"
-            />
-            <iframe
-              className="padding-left"
-              width="500px"
-              height="400px"
-              frameBorder="0"
-              src="https://www.youtube.com/embed/G7-pGlG1DB0"
-            />
+            <iframe width="500px" height="400px" frameBorder="0" src="https://www.youtube.com/embed/wk5GSPw_zX8" />
+            <iframe className="padding-left" width="500px" height="400px" frameBorder="0" src="https://www.youtube.com/embed/G7-pGlG1DB0" />
           </div>
           <div className="block flex-row-center">
-            <iframe
-              width="500px"
-              height="400px"
-              frameBorder="0"
-              src="https://www.youtube.com/embed/aAT5wsRrjFs"
-            />
-            <iframe
-              className="padding-left"
-              width="500px"
-              height="400px"
-              frameBorder="0"
-              src="https://www.youtube.com/embed/r3xmEm75MUE"
-            />
+            <iframe width="500px" height="400px" frameBorder="0" src="https://www.youtube.com/embed/aAT5wsRrjFs" />
+            <iframe className="padding-left" width="500px" height="400px" frameBorder="0" src="https://www.youtube.com/embed/r3xmEm75MUE" />
           </div>
         </div>
         <div className="box">
@@ -94,29 +65,16 @@ class LondonAthletic extends React.Component {
           </div>
           <div className="block flex-row-center">
             <img className="highschool-photos" src={window.images.coleman} />
-            <img
-              className="padding-left highschool-photos"
-              src={window.images.dubois}
-            />
-            <img
-              className="highschool-photos padding-left"
-              src={window.images.fitzpatrick}
-            />
+            <img className="padding-left highschool-photos" src={window.images.dubois} />
+            <img className="highschool-photos padding-left" src={window.images.fitzpatrick} />
           </div>
           <div className="block flex-row-center">
             <img className="highschool-photos" src={window.images.kg_gray} />
-            <img
-              className="padding-left highschool-photos"
-              src={window.images.lewis}
-            />
-            <img
-              className="padding-left highschool-photos"
-              src={window.images.vassel}
-            />
+            <img className="padding-left highschool-photos" src={window.images.lewis} />
+            <img className="padding-left highschool-photos" src={window.images.vassel} />
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
