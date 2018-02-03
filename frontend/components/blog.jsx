a7a009bd9beaf2cfd4ebd8f1a6eb4b05e242b3cf
John Baek <johnbaek92@gmail.com>
2018-02-03 12:52:57 -0500

    complete first blog post

--- frontend/components/blog.jsx
+++ frontend/components/blog.jsx
@@ -42,6 +42,8 @@ class Blog extends React.Component {
         <h1 className="title is-1 has-text-centered" style={{ color: "#1b00ff" }}>
           My Top Moments From Super Bowl 42
         </h1>
+        <img src={window.images.b1} style={{ width: "50%", margin: "0 auto", display: "block" }} />
+        <br />
         <div>
           Has it been 10 years? This year marks the 10th year anniversary of the Giants historic Super Bowl 42 Championship win over the undefeated New England Patriots. I was only a young rookie on the Giants practice roster but it felt like I had 53-man roster privileges. I hung out with the big names on the team, I got a little <a href="https://nypost.com/2008/01/27/hes-mini-moss/" target="_blank" className="hover-style">
             press and publicity
@@ -53,6 +55,14 @@ class Blog extends React.Component {
         <h2 className="title is-2 has-text-centered" style={{ color: "#1b00ff" }}>
           You have arrived in Arizona…
         </h2>
+        <div className="columns is-mobile">
+          <img className="column" src={window.images.b2} style={{ width: "33%", cursor: "default" }} />
+          <img className="column" src={window.images.b3} style={{ width: "33%", cursor: "default" }} />
+        </div>
+        <div className="columns is-mobile">
+          <img className="column" src={window.images.b4} style={{ width: "33%", cursor: "default" }} />
+          <img className="column" src={window.images.b5} style={{ width: "33%", cursor: "default" }} />
+        </div>
         <div>
           I can’t remember the name of the resort we stayed at but I
           remember it was so damn far from the action lol. The place was
@@ -63,10 +73,11 @@ class Blog extends React.Component {
           there was a BIG Reebok bag full of Super Bowl gear because I was
           an endorsed athlete by Reebok. Christmas gifts for DAYS lol!
         </div>
-        <br />
         <h2 className="title is-2 has-text-centered" style={{ color: "#1b00ff" }}>
           David Tyree’s week
         </h2>
+        <img src={window.images.b6} style={{ width: "50%" }} style={{ width: "50%", margin: "0 auto", display: "block" }} />
+        <br />
         <div>
           David Tyree was the team’s spiritual leader and ‘utility guy’. He
           was a special teams monster and did a lot of the dirty work. That
@@ -78,10 +89,13 @@ class Blog extends React.Component {
           greatest catches in Super Bowl history! I remember thinking ‘GOD
           is REAL’ when he made the catch!
         </div>
+        <img src={window.images.b7} style={{ width: "50%" }} style={{ width: "50%", margin: "0 auto", display: "block" }} />
         <br />
         <h2 className="title is-2 has-text-centered" style={{ color: "#1b00ff" }}>
           All eyes on Stray!
         </h2>
+        <img src={window.images.b8} style={{ width: "50%" }} style={{ width: "50%", margin: "0 auto", display: "block" }} />
+        <br />
         <div>
           Throughout the season, I would constantly ask Michael Strahan
           about how to go about building an off the field brand. I wanted
@@ -96,9 +110,13 @@ class Blog extends React.Component {
           blueprint!”
         </div>
         <br />
+        <img src={window.images.b9} style={{ width: "50%" }} style={{ width: "50%", margin: "0 auto", display: "block" }} />
+        <br />
         <h2 className="title is-2 has-text-centered" style={{ color: "#1b00ff" }}>
           Thank You Kellie Pickler
         </h2>
+        <img src={window.images.b10} style={{ width: "50%" }} style={{ width: "33%", margin: "0 auto", display: "block" }} />
+        <br />
         <div>
           I didn’t do many interviews on media day. I mean, why would
           someone want to interview a Practice Squad guy who hadn’t seen
@@ -107,6 +125,9 @@ class Blog extends React.Component {
           be back and you’ll be doing all the top interviews!” I was an
           ambitious rookie!
         </div>
+        <br />
+        <img src={window.images.b11} style={{ width: "50%" }} style={{ width: "50%", margin: "0 auto", display: "block" }} />
+        <br />
         <div>
           Then I remember getting tapped on the shoulder by a short blonde
           haired woman with a slight country accent, asking to help her get
@@ -123,6 +144,14 @@ class Blog extends React.Component {
         <h2 className="title is-2 has-text-centered" style={{ color: "#1b00ff" }}>
           GAME TIME!
         </h2>
+        <div className="columns is-mobile">
+          <img className="column" src={window.images.b12} style={{ width: "33%", cursor: "default" }} />
+          <img className="column" src={window.images.b13} style={{ width: "33%", cursor: "default" }} />
+        </div>
+        <div className="columns is-mobile">
+          <img className="column" src={window.images.b14} style={{ width: "33%", cursor: "default" }} />
+          <img className="column" src={window.images.b15} style={{ width: "33%", cursor: "default" }} />
+        </div>
         <div>
           I remember seeing Alicia Keys and becoming a ‘fanboy’, running up
           to her to ask for a picture pregame and all the other guys who
@@ -130,8 +159,14 @@ class Blog extends React.Component {
           energy in the tunnel as we were about to run out. Every single
           player on our team knew we were going to win that game. I remember
           Eli warming up on the sideline and having this calmness to him. It
-          was the biggest game of this guys career and he was so poised.{" "}
+          was the biggest game of this guys career and he was so poised.
         </div>
+        <br />
+        <div className="columns is-mobile">
+          <img className="column" src={window.images.b16} style={{ width: "33%", cursor: "default" }} />
+          <img className="column" src={window.images.b17} style={{ width: "33%", cursor: "default" }} />
+        </div>
+        <br />
         <div>
           After we had won, the confetti and photographers littered the
           field. I was trying to sneak as close to the trophy as possible
@@ -145,6 +180,18 @@ class Blog extends React.Component {
         <h2 className="title is-2 has-text-centered" style={{ color: "#1b00ff" }}>
           PARTY TIME!
         </h2>
+        <div className="columns is-mobile">
+          <img className="column" src={window.images.b18} style={{ width: "33%", cursor: "default" }} />
+          <img className="column" src={window.images.b19} style={{ width: "33%", cursor: "default" }} />
+        </div>
+        <div className="columns is-mobile">
+          <img className="column" src={window.images.b20} style={{ width: "33%", cursor: "default" }} />
+          <img className="column" src={window.images.b21} style={{ width: "33%", cursor: "default" }} />
+        </div>
+        <div className="columns is-mobile">
+          <img className="column" src={window.images.b22} style={{ width: "33%", cursor: "default" }} />
+          <img className="column" src={window.images.b23} style={{ width: "33%", cursor: "default" }} />
+        </div>
         <div>
           I remember going back to the team hotel briefly to celebrate with
           the organization, friends, and family. I think we stayed there for
