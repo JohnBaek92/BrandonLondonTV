2933d06ed1cbca8f0e5e7323aa3c86cc5ecd9357
John Baek <johnbaek92@gmail.com>
2018-04-23 13:32:44 -0400

    update about, add goplay60

--- frontend/components/entertainment/Entertainment.jsx
+++ frontend/components/entertainment/Entertainment.jsx
@@ -24,7 +24,12 @@ class Entertainment extends React.PureComponent {
       }
     };
 
-    this.videos = ["unxtRCOsZpE", "BKXLB4-9sfg", "Y9YP2EQJ_wI", "AOmHwHWpdKc", "7WOW3hCIg8U", "G4iZJtXw5ig", "rJ_qxnuKADY", "KLmwpPbTsN0", "KLmwpPbTsN0", "4lRkQZTapcg", "erRJtA8bkVc", "J_MAIrPsmRA", "y1_sZeSDlc8"];
+    this.videos = ["Dxs596KF7-Y", "NXLMJLd3Wbw", "Bqz9m6dhA-M"];
+    this.ontheroad = ["KLmwpPbTsN0", "4lRkQZTapcg", "erRJtA8bkVc", "J_MAIrPsmRA"];
+    this.bestmoves = ["unxtRCOsZpE", "z7unu1gYNwU", "BKXLB4-9sfg", "pfL0FbCBomw", "Y9YP2EQJ_wI", "AOmHwHWpdKc"];
+    this.fanexperience = ["7WOW3hCIg8U", "G4iZJtXw5ig", "rJ_qxnuKADY", "y1_sZeSDlc8"];
+    this.asSeenOnTV = ["Dxs596KF7-Y", "NXLMJLd3Wbw", "Bqz9m6dhA-M"];
+    this.goplay = ["WSmNvV-8uwU"];
     this.changeHeight = this.changeHeight.bind(this);
     this.makeVideo = this.makeVideo.bind(this);
     this.openModal = this.openModal.bind(this);
@@ -117,42 +122,59 @@ class Entertainment extends React.PureComponent {
           <img className="column" src={window.images.e8} />
         </div>
         <div className="title has-text-centered">Videos</div>
-        <div className="subtitle has-text-centered">My Best Moves</div>
+        <div className="subtitle has-text-centered">My Best Moves - Player Interviews</div>
         <div className="columns is-mobile" style={{ height: this.state.top }}>
-          {this.videos
+          {this.bestmoves
             .slice(0, 2)
             .map(link => this.makeVideo(link, "middle"))}
         </div>
         <div className="columns is-mobile" style={{ height: this.state.top }}>
-          {this.videos
+          {this.bestmoves
             .slice(2, 4)
+            .map(link => this.makeVideo(link, "middle"))}
+        </div>
+        <div className="columns is-mobile" style={{ height: this.state.top }}>
+          {this.bestmoves
+            .slice(4, 6)
             .map(link => this.makeVideo(link, "middle"))}
         </div>
         <div className="subtitle has-text-centered">Fan Experience</div>
         <div className="columns is-mobile" style={{ height: this.state.top }}>
-          {this.videos
-            .slice(4, 6)
+          {this.fanexperience
+            .slice(0, 2)
             .map(link => this.makeVideo(link, "middle"))}
         </div>
         <div className="columns is-mobile" style={{ height: this.state.top }}>
-          {this.videos
-            .slice(6, 8)
+          {this.fanexperience
+            .slice(2, 4)
             .map((link, idx) => this.makeVideo(link, "middle", idx))}
         </div>
         <div className="subtitle has-text-centered">On The Road</div>
         <div className="columns is-mobile" style={{ height: this.state.top }}>
-          {this.videos
-            .slice(8, 10)
+          {this.ontheroad
+            .slice(0, 2)
             .map((link, idx) => this.makeVideo(link, "middle", idx))}
         </div>
         <div className="columns is-mobile" style={{ height: this.state.top }}>
-          {this.videos
-            .slice(10, 12)
+          {this.ontheroad
+            .slice(2, 4)
+            .map((link, idx) => this.makeVideo(link, "middle", idx))}
+        </div>
+        <div className="subtitle has-text-centered">Giants Play 60</div>
+        <div className="columns is-mobile" style={{ height: this.state.top }}>
+          {this.goplay
+            .slice(0, 2)
+            .map((link, idx) => this.makeVideo(link, "middle", idx))}
+        </div>
+        <div className="subtitle has-text-centered">As Seen On TV</div>
+        <div className="columns is-mobile" style={{ height: this.state.top }}>
+          {this.asSeenOnTV
+            .slice(0, 2)
             .map((link, idx) => this.makeVideo(link, "middle", idx))}
         </div>
         <div className="columns is-mobile" style={{ height: this.state.top }}>
-          {this.videos
-            .slice(12, 14)
+          {this.asSeenOnTV
+            .slice(2, 4)
             .map((link, idx) => this.makeVideo(link, "middle", idx))}
         </div>
       </div>;
